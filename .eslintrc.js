b76500d60500d24701b46fa33ad6a822612ae203
Melina Carniel <melina.carniel@luizalabs.com>
2021-04-23 18:59:03 -0300

    chore: add husky

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/comma-spacing': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/return await': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
+  }
+}
